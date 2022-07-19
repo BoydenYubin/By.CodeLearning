7af107a1db32a5cda94f934690adeef1a7824480
BoydenYubin <312519932@qq.com>
2022-07-19 20:27:36 +0800

    feat(ByLearningThread):添加Task
    1、waitall和whenall的区别

---
+++ src/ByLearningThread/TaskTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ByLearningThread
+{
+    public class TaskTest : ITestWork
+    {
+        public void Run()
+        {
+            CancellationTokenSource cs = new CancellationTokenSource();
+            var task1 = new Task(async token =>
+            {
+                while (!((CancellationToken)token).IsCancellationRequested)
+                {
+                    Console.WriteLine("task1");
+                    Thread.Sleep(100);
+                }
+                await Task.CompletedTask;
+            }, cs.Token);
+            var task2 = new Task(async token =>
+            {
+                while (!((CancellationToken)token).IsCancellationRequested)
+                {
+                    Console.WriteLine("task2");
+                    Thread.Sleep(100);
+                }
+                await Task.CompletedTask;
+            }, cs.Token);
+            task1.Start();
+            task2.Start();
+            var task = Task.WhenAll(task1, task2);  //none block
+            Thread.Sleep(1000);
+            cs.Cancel();
+            Task.WaitAll(task); //block
+            Console.WriteLine("Task all completed!");
+        }
+    }
+}
