e01a2e46f7c3b741cbe230c62f8b4e4b76dfde68
BoydenYubin <312519932@qq.com>
2022-08-02 19:36:17 +0800

    feat(ByLearningID4): 快速构建IdentityServer4代码
    1、新增ID4 nuget package
    2、新增QuickStart UI界面

--- src/ByLearning.IdentityServer4/Startup.cs
+++ src/ByLearning.IdentityServer4/Startup.cs
@@ -16,6 +16,13 @@ namespace ByLearning.IdentityServer4
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddIdentityServer(options =>
+            {
+                //ÅäÖÃIdentityServer4µÄIdentityServerOptions
+            })
+            .AddInitDevelopInfo()
+            .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +33,24 @@ namespace ByLearning.IdentityServer4
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
+
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
