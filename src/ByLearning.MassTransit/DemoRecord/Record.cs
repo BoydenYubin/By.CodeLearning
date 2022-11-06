faf186dcfb44cc0bd095340dc5ba437a246ebe0d
boydenyol <312519932@qq.com>
2022-11-06 20:43:59 +0800

    feat(ByLearning.Masstransit): 新建Masstransit分支
    1、新增InMemoryMassTransit Bus, 了解基础EventBus运行原理(添加Console输出log日志)
        1.1、 注意需要安装8.0及以上版本，8.0以下版本消费不到消息
    2、新增Masstransit.mdj类图
        2.1、 了解Masstransit数据转移详细数据流(简单运行原理了解，即Bus如何转移信息)

---
+++ src/ByLearning.MassTransit/DemoRecord/Record.cs
@@ -0,0 +1,54 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ByLearning.MassTransit.DemoRecord
+{
+    public class OrderCreatedEvent
+    {
+        public Guid OrderId { get; set; }
+    }
+
+    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    {
+        private readonly ILogger<OrderCreatedEventConsumer> _logger;
+        public OrderCreatedEventConsumer(ILogger<OrderCreatedEventConsumer> logger)
+        {
+            _logger = logger;
+        }
+        public Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            _logger.LogInformation($"Received Order:{context.Message.OrderId}");
+            _logger.LogInformation($"- - - - - - - - - - - - - - - - - - - - - -{DateTimeOffset.Now}");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Worker : BackgroundService
+    {
+        private readonly IBus _bus; //注册总线
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(IBus bus, ILogger<Worker> logger)
+        {
+            _bus = bus;
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //模拟并发送订单创建事件
+                Guid id = Guid.NewGuid();
+                await _bus.Publish(new OrderCreatedEvent() { OrderId = id }, stoppingToken);
+                _logger.LogInformation($"Published Order:{id}");
+                //时间间隔1s
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ src/ByLearning.MassTransit/Program.cs
@@ -0,0 +1,50 @@
+﻿using ByLearning.MassTransit.DemoRecord;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using IHost = Microsoft.Extensions.Hosting.IHost;
+using Microsoft.Extensions.Logging;
+
+namespace ByLearning.MassTransit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Masstransit Event Bus";
+            IHost host = Host.CreateDefaultBuilder(args)
+                             .ConfigureServices(services =>
+                             {
+                                 //工作线程，用于模拟发送消息
+                                 services.AddHostedService<Worker>();
+                                 //AddMassTransit 用于添加Masstransit相关依赖项
+                                 services.AddMassTransit(configurator =>
+                                 {
+                                     //注册消费者
+                                     configurator.AddConsumer<OrderCreatedEventConsumer>();
+                                     //使用基于内存的消息路由传输
+                                     configurator.UsingInMemory((context, cfg) =>
+                                     {
+                                         //主要用于配置接受节点ReceiveEndpoint
+                                         cfg.ConfigureEndpoints(context);
+                                     });
+                                 });
+                             })
+                             
+                             .ConfigureLogging(builder =>
+                             {
+                                 builder.AddConsole(options =>
+                                 {
+                                     options.LogToStandardErrorThreshold = LogLevel.Warning;
+                                     options.TimestampFormat = "hh:mm:ss-fff";
+                                 });
+                             })
+                           .Build();
+
+            await host.RunAsync();
+            Console.ReadLine();
+        }
+    }
+}
