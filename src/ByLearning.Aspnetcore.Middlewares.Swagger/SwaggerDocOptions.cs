a8da7ad539e505d2b2a01eba9f55368c6fb04674
boydenyol <312519932@qq.com>
2022-12-26 21:24:47 +0800

    [feat]ByLearning.Aspnetcore.Middleware:新增Swagger API文档 1、service collection注入 2、applicationbuilder中间件两部分 (需根据实际情况调整使用)

---
+++ src/ByLearning.Aspnetcore.Middlewares.Swagger/SwaggerDocOptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ByLearning.Aspnetcore.Middlewares.Swagger
+{
+    public class SwaggerDocOptions
+    {
+        public static SwaggerDocOptions Default => new SwaggerDocOptions()
+        {
+            Version = "1.0.0",
+            Title = "Default Swagger API Docs",
+            Description = "Default Swagger API Docs, if you want to chanage it, make new object as SwaggerDocOptions",
+            TermsOfService = new Uri("http://sample.TermsOfService.com"),
+            Contact = new SwaggerDocContactOptions()
+            {
+                Name = "ByLearning",
+                Email = "bylearning@gmail.com",
+                Uri = new Uri("www.github.com/bylearning")
+            },
+            License = new SwaggerDocLicenseOptions()
+            {
+                Name = "ByLearning",
+                Uri = new Uri("www.github.com/bylearning")
+            }
+        };
+        public string Version { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Uri TermsOfService { get; set; }
+        public SwaggerDocContactOptions Contact { get; set; }
+
+        public SwaggerDocLicenseOptions License { get; set; }
+    }
+
+    public class SwaggerDocContactOptions
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Uri Uri { get; set; }
+    }
+
+    public class SwaggerDocLicenseOptions
+    {
+        public string Name { get; set; }
+        public Uri Uri { get; set; }
+    }
+}
---
+++ src/ByLearning.Aspnetcore.Middlewares.Swagger/SwaggerServicesCollectionExtensions.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace ByLearning.Aspnetcore.Middlewares.Swagger
+{
+    public static class SwaggerServicesCollectionExtensions
+    {
+        /// <summary>
+        /// If you want to ignore some API, you should add attribute [ApiExplorerSettings(IgnoreApi = true)] on your API
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddSwaggerDocs(this IServiceCollection services, [AllowNull] SwaggerDocOptions options)
+        {
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("", new OpenApiInfo()
+                {
+                    Version = options?.Version ?? SwaggerDocOptions.Default.Version,
+                    Title = options?.Title ?? SwaggerDocOptions.Default.Title,
+                    Description = options?.Description ?? SwaggerDocOptions.Default.Description,
+                    TermsOfService = options?.TermsOfService ?? SwaggerDocOptions.Default.TermsOfService,
+                    License = new OpenApiLicense
+                    {
+                        Name = options?.License?.Name ?? SwaggerDocOptions.Default.License.Name,
+                        Url = options?.License?.Uri ?? SwaggerDocOptions.Default.License.Uri
+                    },
+                    Contact = new OpenApiContact()
+                    {
+                        Name = options?.Contact?.Name ?? SwaggerDocOptions.Default.Contact.Name,
+                        Email = options?.Contact?.Email ?? SwaggerDocOptions.Default.Contact.Email,
+                        Url = options?.Contact?.Uri ?? SwaggerDocOptions.Default.Contact.Uri
+                    }
+                }); ;
+                //Should ignore the suppress warnings of 1591
+                //add that on Visualstudio to ignore the warnings
+                var basePath = AppContext.BaseDirectory;
+                //controller xml path, to add comments
+                var xmlPath = Path.Combine(basePath, "");
+                config.IncludeXmlComments(xmlPath);
+                //model xml path, to add comments for model
+                var modelPath = Path.Combine(basePath, "");
+                config.IncludeXmlComments(modelPath);
+
+                #region Oauth if use
+                //config.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                //{
+                //    Type = SecuritySchemeType.OAuth2,
+                //    Flows = new OpenApiOAuthFlows()
+                //    {
+                //        Implicit = new OpenApiOAuthFlow()
+                //        {
+                //            AuthorizationUrl = new Uri($""),
+                //            TokenUrl = new Uri(""),
+                //            Scopes = new Dictionary<string, string>()
+                //            {
+                //                { "XXX", "XXXXX" }
+                //            }
+                //        }
+                //    }
+                //});
+                //config.OperationFilter<AuthorizeCheckOperationFilter>();
+                #endregion
+
+            });
+            return services;
+        }
+        /// <summary>
+        /// Only deploy the swagger UI in development environment!
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseSwaggerUIMiddle(this IApplicationBuilder app)
+        {
+            var env = app.ApplicationServices.GetService<IWebHostEnvironment>();
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+                    // c.IndexStream to change the index.html for swagger
+                    c.RoutePrefix = "";  //if the route path was set empty string, webUI can viewed by base Url+index.html
+                });
+            }
+            return app;
+        }
+    }
+}
